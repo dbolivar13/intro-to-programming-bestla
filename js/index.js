8ff2c35f135ef253556a617380dc024261031b94
David Bolivar <davidbolivar56@gmail.com>
2022-08-14 18:50:44 -0400

    Modified index.html and index.js

--- js/index.js
+++ js/index.js
@@ -16,3 +16,33 @@ for (i = 0; i < lst_skills.length; i++) {
     skill.innerText = lst_skills[i];
     skillsList.appendChild(skill)
 }
+
+// Add event listener for messages
+
+const messageForm = document.querySelector('form');
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const message = e.target.message.value;
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage =  document.createElement('li');
+    newMessage.innerHTML = `<a href={mailto:${email}}>${name}</a> wrote:<span>${message} </span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', (e) => {
+        const entry= e.target.parentNode;
+        messageList.remove(entry);
+    });
+    
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+});
+
+
